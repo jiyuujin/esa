afd7ef8f51b25dc2baf6d5ce3e32e6b64c051c2d
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-09-21 14:46:34 +0900

    lint fix

--- docs/.vuepress/services/auth.js
+++ docs/.vuepress/services/auth.js
@@ -5,6 +5,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 export const setAuth = () => {
     return new ApolloClient({
         link: new HttpLink({
+            /* eslint no-undef: 0 */
             uri: process.env.GRAPH_API || 'https://api.graph.cool/simple/v1/cjr94yoay4hds0196reyj9lke'
         }),
         cache: new InMemoryCache()
