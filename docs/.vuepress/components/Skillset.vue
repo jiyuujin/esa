f59dcfdbbd5c336f1ac6daa402361998dec6c310
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-09-27 12:08:27 +0900

    スキルセットで間のマージンを修正

--- docs/.vuepress/components/Skillset.vue
+++ docs/.vuepress/components/Skillset.vue
@@ -36,40 +36,13 @@ ul {
 }
 
 ul > li {
-  margin-bottom: 60px;
+  margin-bottom: 20px;
 }
 
-@media (min-width : 640px) {
+@media (min-width: 640px) {
   ul > li {
     overflow: hidden;
-    margin: 0;
     position: relative;
-  }
-
-  .work-timeline-date {
-    width: 110px;
-    float: left;
-    margin-top: 20px;
-  }
-
-  .work-timeline-content {
-    width: 75%;
-    float: left;
-    text-align: left;
-    border-left: 3px #35495e solid;
-    padding-left: 30px;
-    font-size: 2vmin;
-  }
-
-  .work-timeline-content:before {
-    content: '';
-    width: 12px;
-    height: 12px;
-    background: #42b883;
-    position: absolute;
-    left: 106px;
-    top: 24px;
-    border-radius: 100%;
   }
 }
 </style>
