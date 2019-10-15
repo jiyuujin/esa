12a66760e705d9c4f32622ef124b83cd49163c88
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-10-15 14:20:13 +0900

    カードコンポーネントを利用するよう変更

--- docs/.vuepress/components/SlideCard.vue
+++ docs/.vuepress/components/SlideCard.vue
@@ -6,23 +6,12 @@
         :key="slide.id"
         class="slide-card"
     >
-      <div class="slide-card-content">
-        <div class="title">
-          {{ slide.title }}
-        </div>
-        <div class="description">
-          {{ formatDate(slide.time) }} {{ slide.event }} で登壇させていただいております。
-        </div>
-      </div>
-      <div class="slide-card-link">
-        <a
-            :href="slide.url"
-            rel="noopener"
-            target="_blank"
-        >
-          Go to Slide!
-        </a>
-      </div>
+      <j-card
+          :title="slide.title"
+          :description="`${formatDate(slide.time)} ${slide.event}`"
+          :link-url="slide.url"
+          link-name="Go to Slide!"
+      ></j-card>
     </div>
   </div>
 </template>
@@ -41,7 +30,7 @@ export default {
   },
   methods: {
     formatDate(d) {
-      return dayjs(d).format('YYYY年 MM月 DD日 dddd')
+      return dayjs(d).format('MM/DD')
     }
   }
 }
@@ -62,51 +51,5 @@ export default {
 
 .slide-card {
   margin: 30px auto;
-  width: 240px;
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px #ccc;
-}
-
-.slide-card-content {
-  padding: 20px;
-}
-
-.slide-card-content .title {
-  font-size: 20px;
-  margin-bottom: 20px;
-  text-align: center;
-  color: #333;
-  overflow: hidden;
-  height: 5.4em;
-  line-height: 1.8;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-align-items: center;
-  vertical-align: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-}
-
-.slide-card-content .description {
-  color: #777;
-  font-size: 14px;
-  line-height: 1.5;
-}
-
-.slide-card-link {
-  text-align: center;
-  border-top: 1px solid #eee;
-  padding: 20px;
-}
-
-.slide-card-link a {
-  text-decoration: none;
-  color: #42b883;
-  margin: 0 10px;
-}
-
-.slide-card-link a:hover {
-  color: #42b883;
 }
 </style>
