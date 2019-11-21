04456da7f1920fbabcdaea94e0845dd031830216
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-11-21 12:51:31 +0900

    ポータル移行準備

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,13 @@
-/* eslint no-undef: 0 */
-module.exports = {
+module.exports = { // eslint no-undef: 0
   title: 'ねこのえさ',
   description: 'Web猫のドキュメント集',
   head: [
-    [
-      'meta',
-      { hid: 'description', name: 'description', content: 'Web猫のドキュメント集' },
-    ],
-    [
-      'meta',
-      { property: 'og:site_name', content: 'ねこのえさ' },
-    ],
-    [
-      'meta',
-      { property: 'og:url', content: 'https://nekohack.app/' },
-    ],
-    [
-      'meta',
-      { property: 'og:type', content: 'website' },
-    ],
-    [
-      'meta',
-      { property: 'og:title', content: 'ねこのえさ' },
-    ],
-    [
-      'meta',
-      { property: 'og:description', content: 'Web猫のドキュメント集' },
-    ],
+    [ 'meta', { hid: 'description', name: 'description', content: 'Web猫のドキュメント集' } ],
+    [ 'meta', { property: 'og:site_name', content: 'ねこのえさ' } ],
+    [ 'meta', { property: 'og:url', content: '' } ],
+    [ 'meta', { property: 'og:type', content: 'website' } ],
+    [ 'meta', { property: 'og:title', content: 'ねこのえさ' } ],
+    [ 'meta', { property: 'og:description', content: 'Web猫のドキュメント集' } ],
   ],
   base: '/',
   docsDir: 'docs',
@@ -43,10 +24,6 @@ module.exports = {
             link: '/docs/',
           },
           {
-            text: 'スクラム',
-            link: '/scrum/',
-          },
-          {
             text: 'ブログ',
             link: 'https://webneko.dev/',
           }
@@ -59,68 +36,12 @@ module.exports = {
               collapsable: false,
               children: [
                 '',
-                'infra',
-                'guide',
                 'community'
-              ]
-            }
-          ],
-          '/scrum/': [
-            {
-              title: 'スクラム',
-              collapsable: false,
-              children: [
-                '',
-                '2019-10-25'
               ]
             }
           ]
         }
-      },
-      // '/en/': {
-      //   repo: 'jiyuujin/esa',
-      //   editLinks: false,
-      //   docsDir: 'docs',
-      //   nav: [
-      //     {
-      //       text: 'Docs',
-      //       link: '/en/docs/',
-      //     },
-      //     {
-      //       text: 'Scrum',
-      //       link: '/en/scrum/',
-      //     },
-      //     {
-      //       text: 'Blog',
-      //       link: 'https://webneko.dev/',
-      //     }
-      //   ],
-      //   sidebarDepth: 3,
-      //   sidebar: {
-      //     '/en/docs/': [
-      //       {
-      //         title: 'Docs',
-      //         collapsable: false,
-      //         children: [
-      //           '',
-      //           'infra',
-      //           'guide',
-      //           'community'
-      //         ]
-      //       }
-      //     ],
-      //     '/en/scrum/': [
-      //       {
-      //         title: 'Scrum',
-      //         collapsable: false,
-      //         children: [
-      //           '',
-      //           '2019-10-25'
-      //         ]
-      //       }
-      //     ]
-      //   }
-      // }
+      }
     }
   },
   locales: {
@@ -129,11 +50,6 @@ module.exports = {
       title: 'ねこのえさ',
       description: 'Web猫のドキュメント集'
     }
-    // '/en/': {
-    //   lang: 'en',
-    //   title: 'neko no esa',
-    //   description: 'Documents of WebNeko'
-    // }
   },
   configureWebpack: {
     resolve: {
